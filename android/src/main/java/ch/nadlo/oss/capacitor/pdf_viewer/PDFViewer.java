68626e0f897a9a602b5df2f6871860958ab1f602
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2025-04-03 17:40:57 +0300

    fix(PDFNavbars): hide android nav bars for native pdf viwer

--- android/src/main/java/ch/nadlo/oss/capacitor/pdf_viewer/PDFViewer.java
+++ android/src/main/java/ch/nadlo/oss/capacitor/pdf_viewer/PDFViewer.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 
 import com.getcapacitor.Bridge;
 import com.rajat.pdfviewer.PdfViewerActivity;
+import com.rajat.pdfviewer.PdfRendererView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +35,16 @@ public class PDFViewer {
                         activeActivities.add(activity);
                         // Add FLAG_KEEP_SCREEN_ON when PdfViewerActivity is created
                         activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+                        View decorView = activity.getWindow().getDecorView();
+                        decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        );
                     }
                 }
 
