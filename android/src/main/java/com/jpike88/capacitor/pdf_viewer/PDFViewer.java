b8d3d378f1391ee7a339f5881d80fb7f448cfc5f
Josh Pike <josh@aquipa.com>
2025-10-27 23:31:58 +0700

    android just turned off

---
+++ android/src/main/java/com/jpike88/capacitor/pdf_viewer/PDFViewer.java
@@ -0,0 +1,11 @@
+package com.jpike88.capacitor.pdf_viewer;
+
+import com.getcapacitor.Logger;
+
+public class PDFViewer {
+
+    public String echo(String value) {
+        Logger.info("Echo", value);
+        return value;
+    }
+}
